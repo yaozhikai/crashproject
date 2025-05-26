7fc68591f002c1596446d7c164c6a32908366167
Zhikai Yao <zya59@uclive.ac.nz>
2025-05-26 22:52:47 +1200

    Review and Refactor: map_plotting.py
    - review and rename parameters and subfunctions for better documentation
    - re-test the shp out put with plotting with map data only
    - the map shape aligns with the main plotting function

--- map_plotting.py
+++ map_plotting.py
@@ -6,7 +6,7 @@ from clean_data import load_and_clean
 MAP_FILE = "data/regional-council-2025.shp"
 SEVERITY_ORDER = ["Fatal Crash", "Serious Crash", "Minor Crash", "Non-Injury Crash"]
 
-def prepare_map_data(df, year):
+def get_region_crash_counts_for_join(df, year):
     """Prepare the DataFrame for map function:
     - plan: include year for filter
     - group by region
@@ -19,13 +19,15 @@ def prepare_map_data(df, year):
     Return:
     DataFrame of crash count by region    
     """
-    filtered_df = df[df['crashYear'] == year] #for later use as filter in streamlit? Choice: == one year or isin year list?
-    grouped = filtered_df.groupby('region', observed= False).size().reset_index(name = 'CrashCount') #use group to accumulate by region name and set column name
+    filtered_df = df.loc[df['crashYear'] == year]  
+    #for later use as filter in streamlit? Choice: == one year or isin year list?
+    grouped = filtered_df.groupby('region', observed= False).size().reset_index(name = 'CrashCount') 
+    #use group to accumulate by region name using size() and set column name as 'CrashCount'
     return grouped 
 
 #note: defalut arguments can be automatically passed to main function
 #note: crash_map_data is not good naming, caused confusion when revisiting the sub-function
-def merge_shp_with_map_data (crash_map_data, shp_path=MAP_FILE, region_key_shp="REGC2025_1", region_key_data="region", count_col="CrashCount"):
+def merge_shp_with_map_data (region_crash_counts, shp_path=MAP_FILE, region_key_shp="REGC2025_1", region_key_data="region", count_col="CrashCount"):
     """Merge map shp data with prepared data for map
     Parameters:
     -introduce shp file
@@ -38,27 +40,31 @@ def merge_shp_with_map_data (crash_map_data, shp_path=MAP_FILE, region_key_shp="
     gpd dataframe merged with crash count, need to filled 0 if no data for the region record.
     """
     gpd_df = gpd.read_file(shp_path)
-    merged = gpd_df.merge(crash_map_data, left_on=region_key_shp, right_on=region_key_data, how="left")
-    merged[count_col] = merged[count_col].fillna(0).astype(int)
+    merged = gpd_df.merge(region_crash_counts, left_on=region_key_shp, right_on=region_key_data, how="left")
+    #why left: maintain integrity of shp data, if data for certain region of a year is 0, nan will be given for the region
+    #right join: if one region is 0, shp will not be introduced and cause error
+    #inner: if one region is 0, shp will not be joined and lose part of region.
+    merged[count_col] = merged[count_col].fillna(0).astype(int) 
+    #fill nan with 0, transfer to int as nan is float
     return merged
 
-"""no use
 def draw_nz_map(shapefile_path=MAP_FILE):
-    
+    """
     Draw a clean static outline map of New Zealand regions using shapefile.
     No labels or index are shown—just geometry for visual inspection or base map use.
 
     Parameters:
         shapefile_path (str): Path to the shapefile (.shp)
-    
+    """
     gdf = gpd.read_file(shapefile_path)
     gdf.plot(color='white', edgecolor='black')
     plt.axis("off")
     plt.tight_layout()
     plt.show()
-"""
 
-def generate_region_crash_map_by_year(df, year, cmap="OrRd"):
+draw_nz_map(shapefile_path=MAP_FILE)
+
+def generate_region_crash_map_by_year(cleaned_df, year, cmap="OrRd"):
     """
     generate a crash count in NZ map for a given year, scalable for streamlit based on scroll bar usage.
     
@@ -69,10 +75,10 @@ def generate_region_crash_map_by_year(df, year, cmap="OrRd"):
 
     To do: need to include data as legend. Will it be able to show the legend dataframe (region & counts) in streamlit dashboard?
     """
-    map_data = prepare_map_data(df, year)
-    merged_gdf = merge_shp_with_map_data(map_data)
+    region_crash_counts = get_region_crash_counts_for_join(cleaned_df, year)
+    merged_gdf = merge_shp_with_map_data(region_crash_counts)
 
-    fig, axes = plt.subplots(figsize=(10, 8))
+    fig, axes = plt.subplots(figsize=(10, 8)) #setup canvas and axes in canvas
     merged_gdf.plot(
         column="CrashCount",
         cmap=cmap,
@@ -83,11 +89,11 @@ def generate_region_crash_map_by_year(df, year, cmap="OrRd"):
     )
     axes.set_title(f"Annual Crash Count by Region of {year}")
     axes.axis("off")
-    plt.tight_layout()
+    plt.tight_layout() #auto adjusts the plot layout
     return fig
 
-df = load_and_clean()
+cleaned_df = load_and_clean()
 
 test_year = 2023
-fig = generate_region_crash_map_by_year(df, test_year, cmap="OrRd")
+fig = generate_region_crash_map_by_year(cleaned_df, test_year, cmap="OrRd")
 plt.show()
